634af5d12ea20abf0b1bdba5c6e94afbf10f9bd7
« <emmajouffroypro@gmail.com>
2020-11-29 23:00:54 +0100

    cleaning code

--- data/load_data.py
+++ data/load_data.py
@@ -9,20 +9,20 @@ from tensorflow.compat.v1.data import make_one_shot_iterator
 filename = '/Users/jouffroy/Desktop/thèse/UNET_repro/data/images.tfrecords'
 batch_size = 5
 
-dataset = dataset(filename)
-batched_dataset = dataset.batch(batch_size)
-iterator = tf.compat.v1.data.make_one_shot_iterator(batched_dataset)
-data = iterator.get_next()
-img = np.array(data['image'][0])
-mask = np.array(data['mask'][0, 0, :, :])
-plt.imshow(img)
-plt.show()
-plt.imshow(mask)
-plt.show()
+#t = dataset(filename)
+#batched_dataset = dataset.batch(batch_size)
+#iterator = tf.compat.v1.data.make_one_shot_iterator(batched_dataset)
+#data = iterator.get_next()
+#img = np.array(data['image'][0])
+#mask = np.array(data['mask'][0, 0, :, :])
+#plt.imshow(img)
+#plt.show()
+#plt.imshow(mask)
+#plt.show()
 
 def load_dataset(filename, batch_size):
-    dataset = decode_data.dataset(filename)
-    batched_dataset = dataset.batch(batch_size)
+    ds = dataset(filename)
+    batched_dataset = ds.batch(batch_size)
     iterator = tf.compat.v1.data.make_one_shot_iterator(batched_dataset)
     return iterator
 
